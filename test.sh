f324bfc565b5412d5d357b0c3da4536e4bde509c
root <root@control.streetrack.org>
2024-11-15 00:28:56 -1000

    updated logs

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test_john
-#SBATCH --output=/scratch/john_result.log
-#SBATCH --error=/scratch/john_error.log
+#SBATCH --output=/home/slurm/john_result.log
+#SBATCH --error=/home/slurm/john_error.log
 #SBATCH --nodelist=node1
 
 /opt/john/run/john --format=raw-sha256 --rules=Jumbo --fork=2 --wordlist=/opt/john/run/password.lst /home/slurm/john_hash.txt
