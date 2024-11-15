642af9764c24633e772b493c785f2955972d47d4
root <root@control.streetrack.org>
2024-11-15 13:47:07 -1000

    updated test scripts

---
+++ main_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=main_test_john
+#SBATCH --output=/home/slurm/main_result.log
+#SBATCH --error=/home/slurm/main_error.log
+#SBATCH --nodelist=node1
+
+/opt/john/run/john --format=raw-sha256 --rules=Jumbo --fork=2 --wordlist=/opt/john/run/password.lst /home/slurm/main_hash.txt
+
