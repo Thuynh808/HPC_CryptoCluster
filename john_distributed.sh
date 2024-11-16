e5c8ddfb077ac938645a02da6222886275e06f94
root <root@control.streetrack.org>
2024-11-16 02:07:02 -1000

    updated john_distributed

---
+++ john_distributed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=john_distributed_test
+#SBATCH --output=/home/slurm/john_distributed_result_%j.log
+#SBATCH --error=/home/slurm/john_distributed_error_%j.log
+#SBATCH --nodes=3
+#SBATCH --ntasks=3
+
+
+WORDLISTS=(/scratch/john/run/passwordlist_00 /scratch/john/run/passwordlist_01 /scratch/john/run/passwordlist_02)
+
+for i in "${!WORDLISTS[@]}"; do
+    srun --exclusive -N1 -n1 /scratch/john/run/john --format=raw-sha256 --rules=Jumbo --fork=2 --wordlist="${WORDLISTS[$i]}" /home/slurm/john_hash.txt &
+done
+
+wait
+
